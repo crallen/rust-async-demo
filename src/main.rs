4973e564bcc4606d21326a7c3ef2c4cb1dfa213d
Chris Allen <christopher.allen@fsgi.com>
2022-05-01 06:53:05 -0500

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,57 @@
+use std::{future::Future, time::Duration};
+
+use futures::future::join_all;
+use rand::prelude::*;
+use tokio::{time::sleep, sync::watch::{self, Receiver}, select, signal};
+
+#[derive(Clone)]
+enum Command {
+    Stop,
+}
+
+#[tokio::main]
+async fn main() {
+    let mut done = false;
+    let (tx, rx) = watch::channel(Command::Stop);
+
+    let workers = start_workers(3, rx);
+    let workers_future = join_all(workers);
+
+    tokio::pin!(workers_future);
+    
+    while !done {
+        select! {
+            _ = &mut workers_future => {
+                done = true;
+            },
+            _ = signal::ctrl_c() => {
+                _ = tx.send(Command::Stop);
+            }
+        }
+    }
+}
+
+fn start_workers(count: usize, rx: Receiver<Command>) -> Vec<impl Future<>> {
+    let mut futures = Vec::new();
+    for id in 1..=count {
+        futures.push(worker(id, rx.clone()));
+    }
+    futures
+}
+
+async fn worker(id: usize, mut rx: Receiver<Command>) {
+    loop {
+        let mut rng = rand::thread_rng();
+        let t = rng.gen_range(2000..=5000);
+
+        select! {
+            _ = sleep(Duration::from_millis(t)) => (),
+            _ = rx.changed() => {
+                println!("{}: stopping", id);
+                break;
+            }
+        }
+
+        println!("{}: tick", id);
+    }
+}
